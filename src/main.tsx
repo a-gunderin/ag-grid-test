a9ce636e41b4ce815bb644075c24ed86a64fc0bd
Andrei Gunderin <ga.webnotes@gmail.com>
2025-09-11 00:38:46 +0300

    chore: initial setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community';
+import GridExample from './App';
+
+ModuleRegistry.registerModules([AllCommunityModule]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <GridExample />
+  </StrictMode>
+);
