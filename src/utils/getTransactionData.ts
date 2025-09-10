c41212eb035df0bdb2bec4648a5b0c28cd8d412f
Andrei Gunderin <ga.webnotes@gmail.com>
2025-09-11 02:10:36 +0300

    chore: setting table

--- src/utils/getTransactionData.ts
+++ src/utils/getTransactionData.ts
@@ -4,6 +4,7 @@ import deals_data from '../data/deals.json';
 export const getTransactionTypeById = (id: number) =>
   transaction_types_data.find((type) => type.id === id)?.name || 'Unknown Type';
 
-export const getDealByInstrumentId = (id: number) =>
-  deals_data.find((deal) => (deal.instrumentIds as number[]).includes(id))
-    ?.instruments[0].name || 'Unknown Deal';
+export const getDealByInstrumentId = (instrumentId: number) =>
+  deals_data.find((deal) =>
+    (deal.instrumentIds as number[]).includes(instrumentId)
+  )?.instruments[0].name || 'Unknown Deal';
