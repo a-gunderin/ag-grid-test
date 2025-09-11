b2ab7aee3b13370cdc66fa8c05bedba6c6865822
Andrei Gunderin <ga.webnotes@gmail.com>
2025-09-11 16:43:23 +0300

    chore: setting table

--- src/utils/getTransactionData.ts
+++ src/utils/getTransactionData.ts
@@ -1,43 +1,40 @@
-import transaction_types_data from '../data/transaction-types.json';
-import deals_data from '../data/deals.json';
-import customers_data from '../data/customers.json';
-import roles_data from '../data/roles.json';
-import loans_data from '../data/loans.json';
-import bank_accounts_data from '../data/bank-accounts.json';
-import transactions_data from '../data/transactions.json';
+import transactionTypes from '../data/transaction-types.json';
+import deals from '../data/deals.json';
+import customers from '../data/customers.json';
+import roles from '../data/roles.json';
+import loans from '../data/loans.json';
+import bankAccounts from '../data/bank-accounts.json';
+import transactions from '../data/transactions.json';
 
 export const getTransactionTypeName = (id: number) =>
-  transaction_types_data.find((type) => type.id === id)?.name || 'Unknown Type';
+  transactionTypes.find((type) => type.id === id)?.name || 'Unknown Type';
 
 export const getDealName = (instrumentId: number) =>
-  deals_data.find((deal) =>
-    (deal.instrumentIds as number[]).includes(instrumentId)
-  )?.name || 'Unknown Deal';
+  deals.find((deal) => (deal.instrumentIds as number[]).includes(instrumentId))
+    ?.name || 'Unknown Deal';
 
 export const getInstrumentName = (instrumentId: number) =>
-  deals_data.find((deal) =>
-    (deal.instrumentIds as number[]).includes(instrumentId)
-  )?.instruments[0].name || 'Unknown Instrument';
+  deals.find((deal) => (deal.instrumentIds as number[]).includes(instrumentId))
+    ?.instruments[0].name || 'Unknown Instrument';
 
 export const getCustomerName = (customerId: number) =>
-  customers_data.find((customer) => customer.id === customerId)?.name ||
+  customers.find((customer) => customer.id === customerId)?.name ||
   'Unknown Customer';
 
 export const getRoleName = (instrumentId: number, customerId: number) => {
-  const deal = deals_data.find((deal) =>
+  const deal = deals.find((deal) =>
     (deal.instrumentIds as number[]).includes(instrumentId)
   );
   const roleId =
     deal?.dealCustomerRoles.find((dcr) => dcr.customerId === customerId)
       ?.roleId || 100;
   const roleName =
-    roles_data.find((role) => role.id === roleId)?.name || 'Unknown Role';
+    roles.find((role) => role.id === roleId)?.name || 'Unknown Role';
   return roleName;
 };
 
 export const getPaymentDate = (instrumentId: number) =>
-  loans_data.find((loan) => loan.id === instrumentId)?.issueDate ||
-  'Unknown Date';
+  loans.find((loan) => loan.id === instrumentId)?.issueDate || 'Unknown Date';
 
 export const formatAmount = (amount: number, currencyCode: string) =>
   new Intl.NumberFormat('en-US', {
@@ -48,7 +45,7 @@ export const formatAmount = (amount: number, currencyCode: string) =>
   }).format(amount);
 
 export const getBankData = (remittanceId?: number) => {
-  const bankAccount = bank_accounts_data.find(
+  const bankAccount = bankAccounts.find(
     (bankAcco) => bankAcco.id === remittanceId
   );
   return {
@@ -62,7 +59,7 @@ export const getHierarchyPath = (transactionId: number) => {
   let currentId: number | undefined = transactionId;
 
   while (currentId !== undefined) {
-    const currentTransaction = transactions_data.find(
+    const currentTransaction = transactions.find(
       (transaction) => transaction.id === currentId
     );
     if (!currentTransaction) break;
